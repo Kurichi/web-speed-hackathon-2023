3852a8c7cd1f0d0580c533f3fd5a63c9b2a6dc79
Kurichi <yuuyapp080@gmail.com>
2023-03-04 22:49:46 +0900

    logo size

--- src/client/components/navigators/Header/Header.tsx
+++ src/client/components/navigators/Header/Header.tsx
@@ -16,7 +16,7 @@ export const Header: FC = () => {
     <header className={styles.container()}>
       <Anchor href="/">
         <div className={styles.logo()}>
-          <Image src="/icons/logo.svg" />
+          <Image height={32} width={205} src="/icons/logo.svg" />
         </div>
       </Anchor>
       {isAuthUser ? (
